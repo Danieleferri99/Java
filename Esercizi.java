7b82c5a2f67d438558b805fad9bb6a56066389db
Danieleferri99 <ferridaniele.1999@gmail.com>
2025-06-06 10:09:30 +0200

    Add files via upload

---
+++ Esercizi.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Esercizi {
+    public static void main(String[] args){
+
+        int base = 0;
+        int altezza = 0;
+        int perimetro = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("inserisci la base: ");
+        base = scanner.nextInt();
+
+        System.out.println("inserisci l'altezza ");
+        altezza = scanner.nextInt();
+
+        perimetro = 2 * (base + altezza); 
+
+        System.out.println("il perimetro è di: " + perimetro);
+
+        scanner.close();
+
+    }
+
+    
+}
