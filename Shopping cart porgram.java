bb6afdf39b93dc792d88a2d4eedadb9c416403c1
Danieleferri99 <ferridaniele.1999@gmail.com>
2025-06-06 13:31:34 +0200

    Update and rename Esercizi.java to Shopping cart porgram.java

--- Shopping cart porgram.java
+++ Shopping cart porgram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Esercizi{
+public class Shopping cart porgram{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
