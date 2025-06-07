70ab6f105f95628645ea9a9f84bce9a38bc8110a
Danieleferri99 <ferridaniele.1999@gmail.com>
2025-06-07 16:10:08 +0200

    Add files via upload

---
+++ Esercizi.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Esercizi {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        String userName;
+        String correctName = "Admin";
+        String password;
+        String correctPasword = "1234";
+
+        System.out.print("Enter Username: ");
+        userName = scanner.nextLine();
+        if (userName.equals(correctName)) {
+            System.out.print("enter you password ");
+            password = scanner.nextLine();
+            if (password.equals(correctPasword)) {
+                System.out.println("login successful ");
+            } else {
+                System.out.println("error: incorrect password ");
+            }
+        } else {
+            System.out.println("error: incorrect username.");
+        }
+        scanner.close();
+    }
+}
