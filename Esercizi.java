7e43427b36d319bb079c5ca26eae89fb52dde82c
Danieleferri99 <ferridaniele.1999@gmail.com>
2025-06-07 14:44:37 +0200

    Add files via upload

---
+++ Esercizi.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Esercizi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float temperature;
+        boolean rain;
+
+        // ask to user
+        System.out.print("what's the temperature today? ");
+        temperature = scanner.nextFloat();
+        if (temperature < 15) {
+            System.out.print("is it raining? (true/false) ");
+            rain = scanner.nextBoolean();
+            if (rain) {
+                System.out.println("wear a jacket and bring an umbrella. ");
+            } else {
+                System.out.println("wear a jacket its cold ");
+            }
+        } else {
+            System.out.print("is it raining? (true/false) ");
+            rain = scanner.nextBoolean();
+            if (rain) {
+                System.out.println("bring an umbrella. ");
+            } else {
+                System.out.println("enjoy the sun! ");
+            }
+        }
+        scanner.close();
+    }
+}
